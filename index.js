2384bb273dfe228523deb3e8975d44f1f78f01bc
Stephen Whitmore <stephen.whitmore@gmail.com>
2015-01-16 09:32:31 -0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,61 @@
+var Twit = require('twit');
+var mkfifo = require('mkfifo').mkfifoSync;
+var fs = require('fs');
+
+var client = new Twit(require('./config'));
+
+// TODO(sww): promises might make this more pleasant
+// Get screen name (for dir name)
+var screenName;
+var timeline;
+client.get('account/verify_credentials', function(err, res) {
+  if (err) throw err;
+  screenName = '@' + res.screen_name;
+
+  // Create dir
+  try {
+    fs.mkdirSync(screenName);
+  } catch (e) {
+    if (e.code !== 'EEXIST') {
+      throw e;
+    }
+  }
+
+  // Create timeline fifo
+  try {
+    mkfifo(screenName + '/timeline', 0644);
+  } catch (e) {
+    if (e.code !== 'EEXIST') {
+      throw e;
+    }
+  }
+
+  // Write stream
+  timeline = fs.createWriteStream(screenName + '/timeline');
+  (function() {
+    var stream = client.stream('user');
+    stream.on('tweet', function (tweet) {
+      timeline.write('@' + tweet.user.screen_name + ': ' + tweet.text + '\n');
+      console.log('tweet');
+    })
+    stream.on('connected', function() {
+      console.log('connected');
+    });
+    stream.on('disconnect', function() {
+      console.log('disconnected');
+    });
+    stream.on('friends', function(msg) {
+      // console.dir(msg.friends);
+    });
+  })();
+});
+
+
+// var following;
+
+// client.get('followers/ids', function(err, reply) {
+//   if(err) return callback(err);
+
+//   following = reply.ids;
+// });
+
